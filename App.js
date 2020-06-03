68e1af9aca35e018b941fc6c06694b605668562d
yasuda <yasudashoyo@yahoo.co.jp>
2020-06-03 23:37:50 +0900

    style memodetail

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
     return (
         <View style={styles.container}>
             <Appbar />
-            <MemoListScreen />
+            <MemoDetailScreen />
         </View>
     );
 }
